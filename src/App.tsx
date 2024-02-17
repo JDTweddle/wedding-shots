02d0477f2ef068bc61370aeada05d26b940551f3
JDTweddle <jdtweddle@gmail.com>
2024-02-17 13:55:32 +0000

    changing entire code base

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
+import AWS from 'aws-sdk';
 
-function App() {
+const s3 = new AWS.S3({
+  accessKeyId: 'YOUR_ACCESS_KEY_ID',
+  secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+  region: 'YOUR_REGION'
+});
+
+const bucketName = 'YOUR_BUCKET_NAME';
+
+const App: React.FC = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      console.log('Selected File:', file.name);
+    }
+  };
+
+  const handleSubmit = async () => {
+    if (selectedFile) {
+      try {
+        const params = {
+          Bucket: bucketName,
+          Key: selectedFile.name,
+          Body: selectedFile,
+          ACL: 'public-read', // Set ACL for public access
+        };
+        const response = await s3.upload(params).promise();
+        console.log('File uploaded successfully:', response.Location);
+        // Now you have the URL of the uploaded image in response.Location
+        alert('File uploaded successfully!');
+      } catch (error) {
+        console.error('Error uploading file:', error);
+        alert('Error uploading file. Please try again.');
+      }
+    } else {
+      alert('Please select a file to upload.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="tc">
+      <h1 className="f2">Becca & David</h1>
+      <p>Please select media to upload</p>
+      <div className="pa4">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleFileChange}
+          className="input-reset ba b--black-20 pa2 mb2 db w-100"
+        />
+        <button
+          onClick={handleSubmit}
+          className="f6 link dim br-pill ph3 pv2 mb2 dib white bg-dark-green"
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </button>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
